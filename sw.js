7869feef736f26f92f41900a0d6cf807373b6910
dburns0024-dot <dburns0024@gmail.com>
2026-04-26 14:09:22 -0700

    Add service worker and PWA icons

--- sw.js
+++ sw.js
@@ -3,31 +3,31 @@
 
 const CACHE = 'brickburst-v1';
 const ASSETS = [
-'./BrickBurst.html',
-'./manifest.json'
+  './BrickBurst.html',
+  './manifest.json'
 ];
 
 // Install: cache all assets
 self.addEventListener('install', e => {
-e.waitUntil(
-caches.open(CACHE).then(cache => cache.addAll(ASSETS))
-);
-self.skipWaiting();
+  e.waitUntil(
+    caches.open(CACHE).then(cache => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
 });
 
 // Activate: clean up old caches
 self.addEventListener('activate', e => {
-e.waitUntil(
-caches.keys().then(keys =>
-Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-)
-);
-self.clients.claim();
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
 });
 
 // Fetch: serve from cache, fall back to network
 self.addEventListener('fetch', e => {
-e.respondWith(
-caches.match(e.request).then(cached => cached || fetch(e.request))
-);
+  e.respondWith(
+    caches.match(e.request).then(cached => cached || fetch(e.request))
+  );
 });
